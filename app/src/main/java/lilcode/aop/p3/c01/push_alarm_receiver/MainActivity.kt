5e3c3c0317b0a796d0c2bc9265c5eecf0ee6ecd7
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-31 15:55:45 +0900

    Initial commit

---
+++ app/src/main/java/lilcode/aop/p3/c01/push_alarm_receiver/MainActivity.kt
@@ -0,0 +1,11 @@
+package lilcode.aop.p3.c01.push_alarm_receiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
